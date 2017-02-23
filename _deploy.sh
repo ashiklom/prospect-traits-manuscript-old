75f7fd6fe494066159b11e8afc245305cecdbf1a
Alexey Shiklomanov <ashiklom@bu.edu>
2017-02-23 11:16:24 -0500

    Add more to introduction and results. Minor build revisions

--- _deploy.sh
+++ _deploy.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 pushd _book
-rm -rf *.git
+rm -rf .git
 git init
-git add * .*
-git commit -f "Create website: `date`"
+git add * .nojekyll
+git commit -m "Create website: `date`"
 git checkout -b gh-pages
 git push -f git@github.com:ashiklom/prospect-traits-manuscript gh-pages
 popd
