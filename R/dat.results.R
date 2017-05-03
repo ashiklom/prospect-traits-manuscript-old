9e6d371ed538c3b8c258be6e343ad2c7561cd2a9
Alexey Shiklomanov <ashiklom@bu.edu>
2017-05-03 16:43:43 -0400

    Modularize remake file. Add traits & results funs

---
+++ R/dat.results.R
@@ -0,0 +1,56 @@
+dat.results_raw_long <- function(specdb_path, resultsdb_path) {
+    library(tidyverse)
+    specdb <- src_sqlite(specdb_path)
+    src_sqlite(resultsdb_path) %>% 
+        tbl('results') %>% 
+        select(-resultid) %>% 
+        collect(n = Inf) %>% 
+        mutate(modelname = factor(modelname, models),
+               parameter = factor(parameter, params('D', TRUE))) %>% 
+        verify(is_unique(samplecode, modelname, parameter))
+}
+
+dat.results_all_long <- function(results_raw_long, spectra_info) {
+    left_join(results_raw_long, spectra_info) %>% 
+        verify(is_unique(samplecode, modelname, parameter))
+}
+
+dat.results_all_wide <- function(results_raw_long, spectra_info) {
+    value_suffix <- c('mean', 'sd', 'q025', 'q500', 'q975')
+    value_vars <- paste0('parameter', value_suffix)
+    names(value_vars) <- value_suffix
+    model_shorten <- function(modelname) gsub('PROSPECT ', '', modelname)
+
+    results_all_wide <- results_raw_long %>% 
+        rename_(.dots = value_vars) %>% 
+        mutate(modelname = fct_relabel(modelname, model_shorten)) %>% 
+        data.table::setDT(.) %>% 
+        data.table::dcast(... ~ parameter + modelname, value.var = value_suffix) %>% 
+        as_data_frame() %>% 
+        left_join(spectra_info) %>% 
+        verify(is_unique(samplecode))
+
+    return(results_all_wide)
+}
+
+# averaged by species
+dat.results_bysp <- function(results_wide) {
+    results_wide %>% 
+        group_by(speciescode) %>% 
+        summarize_at(vars(starts_with('mean'),
+                          -matches('deviance|residual|neff')),
+                     mean) %>% 
+        ungroup()
+}
+
+# Residuals on parametermeans
+dat.results_species_residuals <- function(results_wide) {
+    results_wide %>% 
+        group_by(speciescode) %>% 
+        mutate_at(vars(starts_with('mean')), function(x) x - mean(x)) %>% 
+        ungroup()
+}
+
+dat.filter_results <- function(dat, model) {
+    filter(dat, modelname == model)
+}
---
+++ R/dat.traits.R
@@ -0,0 +1,70 @@
+convert_traits_units <- function(value, trait) {
+    case_when(grepl('(chl|car).*_per_area', trait) ~
+                udunits2::ud.convert(value, 'kg m-2', 'ug cm-2'),
+              grepl('(water_thickness|mass_per_area)', trait) ~ 
+                udunits2::ud.convert(value, 'kg m-2', 'g cm-2'),
+              TRUE ~ value)
+}
+
+dat.traits_raw_long <- function(specdb_path) {
+    specdb <- src_sqlite(specdb_path)
+    tbl(specdb, 'trait_data') %>% 
+        select(samplecode, trait, traitvalue) %>% 
+        filter(samplecode != 'nasa_fft|PB02_ABBA_TN|2008') %>% 
+        collect(n = Inf) %>% 
+        mutate(trait = factor(trait),
+               traitvalue = convert_traits_units(traitvalue, trait)) %>% 
+        ## TODO: Fix this in curated leafspec post-processing
+        group_by(samplecode, trait) %>% 
+        summarize(traitvalue = mean(traitvalue, na.rm = TRUE)) %>% 
+        ungroup() %>% 
+        verify(is_unique(samplecode, trait))
+}
+
+dat.traits_raw_wide <- function(traits_raw_long) {
+    spread(traits_raw_long, trait, traitvalue)
+}
+
+dat.traits_all_long <- function(results_all_long) {
+    results_all_long %>% 
+        inner_join(traits_raw_long) %>% 
+        verify(is_unique(samplecode, modelname, parameter, trait))
+}
+
+dat.traits_all_wide <- function(results_all_wide) {
+    results_all_wide %>% 
+        inner_join(traits_raw_wide) %>% 
+        verify(is_unique(samplecode))
+}
+
+dat.traits_long <- function(results_long, traits_raw_long) {
+    results_long %>% 
+        inner_join(traits_raw_long) %>% 
+        verify(is_unique(samplecode, parameter, trait))
+}
+
+dat.traits_wide <- function(results_wide, traits_raw_wide){
+    results_wide %>% 
+        inner_join(traits_raw_wide) %>% 
+        verify(is_unique(samplecode))
+}
+
+dat.traits_all_valid_long <- function(traits_all_long) {
+    traits_all_long %>% 
+        filter((parameter == 'Cab' & trait == 'leaf_chltot_per_area') |
+               (parameter == 'Car' & trait == 'leaf_cartot_per_area') |
+               (parameter == 'Cw' & trait == 'leaf_water_thickness') |
+               (parameter == 'Cm' & trait == 'leaf_mass_per_area')) %>% 
+        group_by(parameter, modelname) %>% 
+        mutate(resid = parametermean - traitvalue,
+            normresid = resid / traitvalue,
+            scaledresid = resid / mean(traitvalue)) %>% 
+        ungroup() %>% 
+        verify(is_unique(samplecode, modelname, parameter, trait))
+}
+
+dat.traits_valid_long <- function(traits_all_valid_long, model) {
+    traits_all_valid_long %>% 
+        filter(modelname == model) %>% 
+        verify(is_unique(samplecode, parameter, trait))
+}
